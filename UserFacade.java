d9ac8b8ca330efdee408f1f7eede573b058e13df
Ismael Lima <130599536+Ismaellima4@users.noreply.github.com>
2024-02-19 11:58:17 -0300

    Create UserFacade.java

---
+++ UserFacade.java
@@ -0,0 +1,10 @@
+package blog;
+
+public interface UserFacade {
+    void login();
+    void sair();
+    void acessarArtigo();
+    void acessarArtigos();
+    void acessarNoticias();
+    void acessarNoticia();
+}
