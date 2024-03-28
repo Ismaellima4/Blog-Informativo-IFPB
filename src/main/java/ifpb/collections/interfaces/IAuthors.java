4e335af9ecd8e38500f8a028e44690c645914ead
Pedro Gregório <pedrosgregorio@gmail.com>
2024-03-28 00:44:13 -0300

    Finalizando interface

--- src/main/java/ifpb/collections/interfaces/IAuthors.java
+++ src/main/java/ifpb/collections/interfaces/IAuthors.java
@@ -1,4 +1,12 @@
 package ifpb.collections.interfaces;
 
+import ifpb.entitybasic.interfaces.IAuthor;
+import ifpb.entitybasic.interfaces.IID;
+
 public interface IAuthors {
+    void add(IAuthor author);
+    void remove(IID id);
+    IAuthors list();
+    void update(IID id, IAuthor author);
+    IAuthor get(IAuthor author);
 }
