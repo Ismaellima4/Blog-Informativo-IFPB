b2b38329673ae13f204c3b706587ac9cc24fc13e
Ismael Lima <130599536+Ismaellima4@users.noreply.github.com>
2024-04-03 20:46:24 -0300

    Update methods IInformation.java

--- src/main/java/ifpb/entitybasic/interfaces/IInformation.java
+++ src/main/java/ifpb/entitybasic/interfaces/IInformation.java
@@ -3,4 +3,7 @@ package ifpb.entitybasic.interfaces;
 public interface IInformation {
     void changeTitle(String title) throws Exception;
     void changeDescription(String description) throws Exception;
+    void validateWhiteSpaces(String content) throws Exception;
+    String validateTitle(String title) throws Exception;
+    String validateDescription(String description) throws Exception;
 }
