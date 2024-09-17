8d195eeb11aac88f0aff9d89bff6fc2218b8b479
ismaellima4 <ismael.github@gmail.com>
2024-09-17 20:16:49 -0300

    Delete file

--- src/main/java/ifpb/UserManager.java
+++
@@ -1,8 +0,0 @@
-package ifpb;
-
-public interface UserManager {
-    String singIn(String email,String pasword);
-    String signUp(String name,String email,String password,String username);
-    void removeUser(int id);
-    boolean update(int id, String name, String email, String password,String username);
-}
