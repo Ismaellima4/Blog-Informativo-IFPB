608ec02bd501d4071fec1ec1b373eb279f12daa9
lrickelme <rickelmelucas200906@gmail.com>
2024-09-18 21:33:20 -0300

    correcting class UserRepositoryImplementation and User Collection, delete UserList and UserManager but many modifications are missing

--- src/main/java/ifpb/UserManager.java
+++
@@ -1,8 +0,0 @@
-package ifpb;
-
-public interface UserManager {
-    String signIn(String email, String password); // Corrigido
-    String signUp(String name, String email, String password, String username); // Corrigido
-    void removeUser(int id);
-    boolean update(int id, String name, String email, String password, String username);
-}
